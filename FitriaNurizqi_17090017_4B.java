bb49a3446ecd76e033acfc177246b137148dfd90
fitriarizky2000 <48342537+fitriarizky2000@users.noreply.github.com>
2019-03-07 20:41:32 -0800

    Add files via upload

---
+++ FitriaNurizqi_17090017_4B.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kiki
+ */
+public class FitriaNurizqi_17090017_4B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama = FitriaNurizqi_17090017_4B"
+                + "NIM = 17090103"
+                + "Kelas = 4B");
+    }
+    
+}
